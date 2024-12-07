01404cd7afee106da4798d733e1804e908aad97f
Stanislav <stasdulusff@gmail.com>
2024-12-07 13:15:39 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{textAlign: 'center', marginTop: '50px'}}>
+      <h1>Привет, это минимальное приложение на React!</h1>
+      <p>Работает в Docker :)</p>
+    </div>
+  );
+}
+
+export default App;
