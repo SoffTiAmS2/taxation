3059b72403fe0d3ead26b1eac42b846da2c80d6b
Stanislav <stasdulusff@gmail.com>
2024-12-07 18:11:04 +0700

    dinamik update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
 import React from 'react';
 
 function App() {
-  return (
-    <div style={{textAlign: 'center', marginTop: '50px'}}>
-      <h1>Привет, это минимальное приложение на React!</h1>
-      <p>Работает в Docker :)</p>
-    </div>
-  );
-}
+    return <h1>ывагшрыфлрвао</h1>;
+  }
 
 export default App;
